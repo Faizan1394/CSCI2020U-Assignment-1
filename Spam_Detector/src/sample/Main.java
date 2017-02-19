a3f03c57305116af87bf90dd90c7e0a891204ef2
Miral <miral.panchal@uoit.net>
2017-02-19 11:57:03 -0500

    Added basic layout for the project

--- Spam_Detector/src/sample/Main.java
+++ Spam_Detector/src/sample/Main.java
@@ -4,15 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Spam Detector 3000");
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File("."));
+        File mainDirectory = directoryChooser.showDialog(primaryStage);
+
+        primaryStage.setScene(new Scene(root, 750, 650));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
